dd3bdd3fa2cd20423823526460345c92500f2758
Dieter Van Meerbeeck <dietervanmeerbeeck@hotmail.com>
2022-10-20 02:24:41 +0200

    Sticky footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,13 +5,13 @@ import FooterLinks from "./FooterLinks";
 export default function Footer(){
   return (
     <>
-      <div id="footerBox">
+      <footer id="footerBox">
         <Container sx={{paddingTop: "15px", paddingBottom: "15px"}}>
           <Grid container direction="row">
             <FooterLinks />
           </Grid>
         </Container>
-      </div>
+      </footer>
     </>
   )
 }
